d429ebe8edc0f8310f95e31054613d7084710883
Altamash Khan <altamashkhan59@gmail.com>
2020-07-19 18:48:45 +0530

    order summary in  making

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,30 @@
+import react from 'react';
+
+
+import Aux from '../../../hoc/Auxi';
+ const ordersummary=(props)=>{
+    const ingredientSummary = Object.keys( props.ingredients )
+    .map( igKey => {
+        return (
+            <li key={igKey}>
+                <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+            </li> );
+    } );
+
+return(
+<Aux>
+<h3>Burger is ready dude or madame</h3>
+<ul>{ingredientSummary}</ul>
+
+
+
+
+
+</Aux>
+
+
+)
+
+
+
+ }
